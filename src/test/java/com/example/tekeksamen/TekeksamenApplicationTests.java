b5ee09b36e0c11d40ea63088b1c4a70bfa3f3692
breelef <emil_vinther@hotmail.com>
2023-01-08 16:08:23 +0100

    Projekt

---
+++ src/test/java/com/example/tekeksamen/TekeksamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tekeksamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TekeksamenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
